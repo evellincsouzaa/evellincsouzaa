01e2c55ea842bd5d190760973fdb4b161b85d254
ruanaribeiro <ruanaribeiro3@gmail.com>
2023-05-15 15:17:05 -0300

    ftt

---
+++ cardapio.php
@@ -0,0 +1,405 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coffe Boutique</title>
+    <link rel="shortcut icon" href="./ico03/lanche.png" type="image/png">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="01estilo.css">
+</head>
+<body>
+
+
+    <img class="steff" src="banner.jpg" width="1510px">
+    <div>
+    <div class="menu">
+        <ul>
+            <li><a href="index.html">home</a></li>
+            <li><a href="contato.html">contato</a></li>
+            <li><a href="cardapio.html">cardápio</a></li>
+            <li><a href="sobre.html"> sobre</a></li>
+        </ul>
+    </div>
+
+    
+ 
+<header>
+    <h1>Bebidas</h1>
+</header>
+<main>
+
+        
+
+    </div>
+</main>
+
+<!------------BEBIDAS---------------------->
+
+
+<style>
+    .gallery-items {
+        display: inline-block;
+        margin-right: 20px; /* Ajuste o espaçamento entre as imagens aqui */
+    }
+</style>
+
+<div class="gallery-items">
+    <img src="Black Summer.jpg" width="80px">
+    <h2>Black Summer</h2>
+    <p>R$25,99</p>
+</div>
+
+<div class="gallery-items">
+    <img src="Dark Moon.jpg" width="80px">
+    <h2>Dark Moon</h2>
+    <p>R$23,90</p>
+</div>
+
+<div class="gallery-items">
+    <img src="Hot N Cold.jpg" width="40px">
+    <h2>Hot N Cold</h2>
+    <p>R$26,90 </p>
+</div>
+
+<div class="gallery-items">
+    <img src="Rainbow Drink.jpg" width="40px">
+    <h2>Rainbow Drink</h2>
+    <p>R$29,99 </p>
+</div>
+
+<div class="gallery-items">
+    <img src="Strawberry Fields Forever.jpg" width="100px">
+    <h2>Strawberry Fields Forever</h2>
+    <p>R$25,99 </p>
+</div>
+
+<div class="gallery-items">
+    <img src="Sunny Day.jpg" width="100px">
+    <h2>Sunny Day</h2>
+    <p>R$27,90 </p>
+</div>
+
+
+<div class="gallery-items">
+    <img src="Drink Chocolate Bomb.jpg" width="100px">
+    <h2>Chocolate Bomb</h2>
+    <p>R$28,90 </p>
+</div>
+
+
+<div class="gallery-items">
+    <img src=" Popcorn Drink.jpg" width="100px">
+    <h2>Popcorn Drink</h2>
+    <p>R$29,90 </p>
+</div>
+
+
+
+
+
+
+
+
+</html>
+</body>
+
+<header>
+    <h1>Doces</h1>
+</header>
+<main>
+
+        
+
+    </div>
+</main>
+
+
+<style>
+    .gallery-items {
+        display: inline-block;
+        margin-right: 20px; /* Ajuste o espaçamento entre as imagens aqui */
+    }
+</style>
+
+<div class="gallery-items">
+    <img src="Bolo de Chocolate Premium.jpg" width="80px">
+    <h2>Bolo de Chocolate Premium</h2>
+    <p>R$20,00</p>
+</div>
+
+<div class="gallery-items">
+    <img src="Caixa de Doces Finos.jpg" width="80px">
+    <h2>Caixa de Doces Finos</h2>
+    <p>R$59,90</p>
+</div>
+
+<div class="gallery-items">
+    <img src="Cookie de Kinder Bueno.jpg" width="40px">
+    <h2>Cookie de Kinder Bueno</h2>
+    <p>R$19,90 </p>
+</div>
+
+<div class="gallery-items">
+    <img src="Croissant com Creme de Leite Ninho e Morangos.jpg" width="40px">
+    <h2>Croissant de Creme</h2>
+    <p>R$29,99 </p>
+</div>
+
+<div class="gallery-items">
+    <img src="Cupcake de Frutas Vermelhas.jpg" width="100px">
+    <h2>Cupcake de Frutas</h2>
+    <p>R$18,99 </p>
+</div>
+
+<div class="gallery-items">
+    <img src="Cupcake de Pistache com Frutas Vermelhas.jpg" width="100px">
+    <h2>Cupcake de Pistache</h2>
+    <p>R$23,90 </p>
+</div>
+
+
+<div class="gallery-items">
+    <img src="Red Velvet.jpg" width="100px">
+    <h2>Red Velvet</h2>
+    <p>R$28,90 </p>
+</div>
+
+
+<div class="gallery-items">
+    <img src="Torta de Maracujá.jpg" width="100px">
+    <h2>Torta de Maracujá</h2>
+    <p>R$25,90 </p>
+</div>
+
+
+
+
+
+
+
+
+</html>
+</body>
+
+
+<header>
+    <h1>Salgados</h1>
+</header>
+<main>
+
+        
+
+    </div>
+</main>
+
+<!------------Salgados---------------------->
+
+
+<style>
+    .gallery-items {
+        display: inline-block;
+        margin-right: 20px; /* Ajuste o espaçamento entre as imagens aqui */
+    }
+</style>
+
+<div class="gallery-items">
+    <img src="bolinho de bacon.jpg" width="80px">
+    <h2>Bolinho de Bacon</h2>
+    <p>R$19,99</p>
+</div>
+
+<div class="gallery-items">
+    <img src="donut de queijo e amendoas.jpg" width="80px">
+    <h2>Donut de Amendoas</h2>
+    <p>R$23,90</p>
+</div>
+
+<div class="gallery-items">
+    <img src="mini pizza italiana.jpg" width="40px">
+    <h2>Mini Pizza Italiana</h2>
+    <p>R$15,90 </p>
+</div>
+
+<div class="gallery-items">
+    <img src="Pao Salgado Recheado de Queijo Cottage e Bacon.jpg" width="40px">
+    <h2>Pão Salgado de Queijo </h2>
+    <p>R$25,90 </p>
+</div>
+
+<div class="gallery-items">
+    <img src="rosca salgada de escarola.jpg" width="100px">
+    <h2>Rosca Salgada de Escarola</h2>
+    <p>R$22,90 </p>
+</div>
+
+<div class="gallery-items">
+    <img src="sanduiche de file mignon e pepino.jpg" width="100px">
+    <h2>Sanduiche de Filé Mignon</h2>
+    <p>R$27,90 </p>
+</div>
+
+
+<div class="gallery-items">
+    <img src="vegan nature.jpg" width="100px">
+    <h2>Vegan Nature</h2>
+    <p>R$18,90 </p>
+</div>
+
+
+<div class="gallery-items">
+    <img src="wrap mexicano.jpg" width="100px">
+    <h2>Wrap Mexicano</h2>
+    <p>R$25,99 </p>
+</div>
+
+
+
+
+
+
+
+
+</html>
+</body>
+
+
+<?php
+
+
+$bebidas = array(
+  array(
+    'nome' => 'Sunny Day',
+    'preco' => 28.90,
+    'imagem' => 'Sunny Day.jpg'
+  ),
+  array(
+    'nome' => 'Popcorn Drink',
+    'preco' => 27.90,
+    'imagem' => 'Popcorn Drink.jpg'
+  ),
+  array(
+    'nome' => 'Dark Moon',
+    'preco' => 16.90,
+    'imagem' => 'Dark Moon.jpg'
+  ),
+  array(
+    'nome' => 'Black Summer',
+    'preco' => 24.00,
+    'imagem' => 'Black Summer.jpg'
+  ),
+  array(
+    'nome' => 'Hot N Cold',
+    'preco' => 28.00,
+    'imagem' => 'Hot N Cold.jpg'
+  ),
+  array(
+    'nome' => 'Strawberry Fields Forever',
+    'preco' => 24.90,
+    'imagem' => 'Strawberry Fields Forever.jpg'
+  ),
+  array(
+    'nome' => 'Drink Chocolate Bomb',
+    'preco' => 24.90,
+    'imagem' => 'Drink Chocolate Bomb.jpg'
+  ),
+  array(
+    'nome' => 'Rainbow Drink',
+    'preco' => 24.90,
+    'imagem' => 'Rainbow Drink.jpg'
+  ),
+  
+);
+
+
+$doces = array(
+  array(
+    'nome' => 'Red Velvet',
+    'preco' => 14.50,
+    'imagem' => 'Red Velvet.jpg'
+  ),
+  array(
+    'nome' => 'Croissant com Creme de Leite Ninho e Morangos',
+    'preco' => 19.00,
+    'imagem' => 'Croissant com Creme de Leite Ninho e Morangos.jpg'
+  ),
+  array(
+    'nome' => 'Cookie de Kinder Bueno',
+    'preco' => 17.90,
+    'imagem' => 'Cookie de Kinder Bueno.jpg'
+  ),
+  array(
+    'nome' => 'Caixa de Doces Finos',
+    'preco' => 25.00,
+    'imagem' => 'Caixa de Doces Finos.jpg'
+  ),
+  array(
+    'nome' => 'Cupcake de Frutas Vermelhas',
+    'preco' => 13.00,
+    'imagem' => 'Cupcake de Frutas Vermelhas.jpg'
+  ),
+  array(
+    'nome' => 'Torta de Maracujá',
+    'preco' => 20.00,
+    'imagem' => 'Torta de Maracujá.jpg'
+  ),
+  array(
+    'nome' => 'Bolo de Chocolate Premium',
+    'preco' => 20.00,
+    'imagem' => 'Bolo de Chocolate Premium.jpg'
+  ),
+  array(
+    'nome' => 'Cupcake de Pistache com Frutas Vermelhas',
+    'preco' => 20.00,
+    'imagem' => 'Cupcake de Pistache com Frutas Vermelhas.jpg'
+  ),
+
+);
+
+$salgados = array(
+  array(
+    'nome' => 'Bolinho de Bacon',
+    'preco' => 12.50,
+    'imagem' => 'bolinho de bacon.jpg'
+  ),
+  array(
+    'nome' => 'Donut de queijo e amendoas',
+    'preco' => 4.00,
+    'imagem' => 'donut de queijo e amendoas.jpg'
+  ),
+  array(
+    'nome' => 'Pão salgado recheado com queijo cottage e bacon',
+    'preco' => 4.00,
+    'imagem' => 'pao salgado recheado de queijo cottage e bacon.jpg'
+  ),
+  array(
+    'nome' => 'Rosca salgada de escarola',
+    'preco' => 10.50,
+    'imagem' => 'rosca salgada de escarola.jpg'
+  ),
+  array(
+    'nome' => 'sanduiche de filé mignon e pepino',
+    'preco' => 9.80,
+    'imagem' => 'sanduiche de file mignon e pepino.jpg'
+  ),
+  array(
+    'nome' => 'Vegan Nature',
+    'preco' => 7.00,
+    'imagem' => 'vegan nature.jpg'
+  ),
+  array(
+    'nome' => 'Wrap Mexicano',
+    'preco' => 7.00,
+    'imagem' => 'wrap mexicano.jpg'
+  ),
+  array(
+    'nome' => 'Mini Pizza Italiana',
+    'preco' => 7.00,
+    'imagem' => 'mini pizza italiana.jpg'
+  ),
+  
+  
+);
+?>
